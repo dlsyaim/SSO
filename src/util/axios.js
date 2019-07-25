94c398b383ebd407e7ac22514ff56db2510936fd
wangbo <707607105@qq.com>
2019-07-25 15:39:11 +0800

    add：新增axios

---
+++ src/util/axios.js
@@ -0,0 +1,114 @@
+import axios from 'axios';
+import Vue from 'vue';
+
+axios.defaults.validateStatus=()=>true;
+
+const accessToken = sessionStorage.getItem('Access-Token')?sessionStorage.getItem('Access-Token'):'';
+
+export const get = (url, params) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'get',
+      url: url,
+      headers: {
+        'Authorization': accessToken
+      },
+      params: params
+    }).then(res => {
+      if (res.status === 1) {
+        resolve(res.data);
+      } else{
+        handleBusinessError(res.data);
+        resolve(res.data);
+      }
+    }).catch(err => {
+      handleHttpError(err);
+      resolve(err.data)
+    })
+  });
+};
+
+export const post = (url, data) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'post',
+      url: url,
+      headers: {
+        "Authorization": accessToken
+      },
+      data: data
+    }).then(res => {
+      if (res.status === 1) {
+        resolve(res.data);
+      } else{
+        handleBusinessError(res.data);
+        resolve(res.data);
+      }
+    }).catch(err => {
+      handleHttpError(err);
+      resolve(err.data)
+    })
+  });
+};
+
+export const put = (url, data) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'put',
+      url: url,
+      headers: {
+        "Authorization": accessToken
+      },
+      data: data
+    }).then(res => {
+      if (res.status === 1) {
+        resolve(res.data);
+      } else{
+        handleBusinessError(res.data);
+        resolve(res.data);
+      }
+    }).catch(err => {
+      handleHttpError(err);
+      resolve(err.data)
+    })
+  });
+};
+
+export const deleteRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: 'delete',
+      url: url,
+      headers: {
+        "Authorization": accessToken
+      }
+    }).then(res => {
+      if (res.status === 1) {
+        resolve(res.data);
+      } else{
+        handleBusinessError(res.data);
+        resolve(res.data);
+      }
+    }).catch(err => {
+      handleHttpError(err);
+      resolve(err.data)
+    })
+  });
+};
+
+
+/**
+ * 处理http请求错误
+ * @param err
+ */
+handleHttpError=(err)=>{
+  Vue.prototype.$message.error('服务器出错');
+};
+
+/**
+ * 处理业务逻辑错误
+ * @param err
+ */
+handleBusinessError=(err)=>{
+  Vue.prototype.$message.error(err.msg);
+};
