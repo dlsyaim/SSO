956c3903860b11a3921b53d909bc72c57da6a399
wangbo <707607105@qq.com>
2019-07-25 15:43:58 +0800

    add：新增常用全局配置

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+/**
+ * api根路径
+ * @type {string}
+ */
+export const BASE_URL='HTTP://10.0.0.95';
+
+/**
+ * 表格常用默认分页配置
+ */
+export const tablePaginationConfig = {
+  current: 1,
+  pageSize: 10,
+  total:0,
+  showSizeChanger: true,
+  showQuickJumper: true,
+};
