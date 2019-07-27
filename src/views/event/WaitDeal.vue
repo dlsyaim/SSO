98d0baf341c008e51c822e513ce395d479061c48
wangbo <707607105@qq.com>
2019-07-27 11:02:53 +0800

    feature-fix：获取表格数据时，将处理数据的函数抽取独立出来

--- src/views/event/WaitDeal.vue
+++ src/views/event/WaitDeal.vue
@@ -3,14 +3,15 @@
     <a-card title="待办问题" :bordered="false" style="min-height: 100%">
       <div style="display: flex;align-items: center">
         <span>问题来源：</span>
-        <a-select class="default-select-width" style="margin-right: 40px" placeholder="请选择问题来源" @change="handleProblemSelectChange">
+        <a-select class="default-select-width" style="margin-right: 40px" placeholder="请选择问题来源" @change="handleProblemSourceSelectChange">
           <a-select-option value="A">河长APP</a-select-option>
           <a-select-option value="S">市平台同步</a-select-option>
         </a-select>
         <span>问题等级：</span>
-        <a-select class="default-select-width" style="margin-right: 40px" placeholder="请选择问题等级" @change="handleProblemSelectChange">
-          <a-select-option value="A">河长APP</a-select-option>
-          <a-select-option value="S">市平台同步</a-select-option>
+        <a-select class="default-select-width" style="margin-right: 40px" placeholder="请选择问题等级" @change="handleProblemLevelSelectChange">
+          <a-select-option value="0">一般</a-select-option>
+          <a-select-option value="1">紧急</a-select-option>
+          <a-select-option value="2">特急</a-select-option>
         </a-select>
         <a-button type="primary" @click="getList">查询</a-button>
       </div>
@@ -28,13 +29,20 @@
 
 <script>
   import {get} from "../../util/axios";
-  import {GET_PROBLEM_LIST} from "../../api/problem";
+  import {GET_PROBLEM_LIST, GET_PROBLEM_TYPE} from "../../api/event";
   import {tablePaginationConfig} from "../../config/config";
+  import {eventStatusEnum} from "../../config/enum";
 
   const columns = [
     {title: '到达时间', dataIndex: 'arrivalTime'},
     {title: '问题来源', dataIndex: 'eventResourceName'},
-    {title: '发现时间', dataIndex: 'createtime'}
+    {title: '问题等级', dataIndex: 'eventLevelName'},
+    {title: '发现时间', dataIndex: 'createtime'},
+    {title: '问题内容', dataIndex: 'content'},
+    {title: '所属区域', dataIndex: 'belongArea'},
+    {title: '问题状态', dataIndex: 'eventStatusName'},
+    {title: '委办单位', dataIndex: 'acceptperson'},
+    {title: '扣分分值', dataIndex: 'deductionScore'}
   ];
   const problemSourceEnum = {
     'A': '河长APP',
@@ -45,23 +53,33 @@
     'G': '钉钉平台',
     'S': '市平台同步'
   };
+  const problemLevelEnum={
+    '0':'一般',
+    '1':'紧急',
+    '2':'特急',
+  };
   export default {
     data() {
       return {
         searchCondition: {
           userId: '',
-          eventResource: null
+          eventResource: null,
+          eventLevel:null
         },
         columns,
         list: [],
         loading: false,
         pagination: tablePaginationConfig,
-        problemSourceEnum
+        problemSourceEnum,
+        problemLevelEnum,
+        eventStatusEnum,
+        problemTypeList:[]
       }
     },
     mounted() {
       this.searchCondition.userId = JSON.parse(sessionStorage.getItem('userDTO')).id;
       this.getList();
+      this.getProblemType();
     },
     methods: {
       getList() {
@@ -84,18 +102,42 @@
         get(GET_PROBLEM_LIST, params).then(res => {
           this.loading = false;
           if (res.resCode === 1) {
-            this.list = res.data.list.map(item=>{
-              item.eventResourceName=this.problemSourceEnum[item.eventresource];
-              return item;
-            });
+            this.handleList(res.data.list);
+            this.pagination.total=res.data.total;
           }
         })
       },
-      handleProblemSelectChange(e) {
-        this.params.eventResource = e;
+      handleList(list){
+        list.forEach(item=>{
+          item.eventResourceName=this.problemSourceEnum[item.eventresource];
+          item.eventLevelName=this.problemLevelEnum[item.eventlevel];
+          item.belongArea=item.eventbelongcity+item.eventbelongcounty+item.eventbelongtown+item.eventbelongvillage;
+          item.eventStatusName=this.eventStatusEnum[item.status];
+        });
+        this.list=list;
+      },
+      getProblemType(){
+        get(GET_PROBLEM_TYPE).then(res=>{
+          if(res.resCode){
+            const problemTypeList=[];
+            res.data.forEach(item=>{
+              if(!item.isParent){
+                problemTypeList.push(item);
+              }
+            })
+            // this.problemTypeList=res.data.map()
+          }
+        });
+      },
+      handleProblemSourceSelectChange(e) {
+        this.searchCondition.eventResource = e;
+      },
+      handleProblemLevelSelectChange(e){
+        this.searchCondition.eventLevel=e;
       },
       handleTableChange(e) {
         this.pagination = e;
+        this.getList();
       }
     }
   }
