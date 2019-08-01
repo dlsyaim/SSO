730fd524922b80f0aa81da7a9836bb5a817c4ed8
wangbo <707607105@qq.com>
2019-08-01 10:40:21 +0800

    feature-fix：区域选择弹出框新增搜索功能

--- src/components/RegionTreeModal.vue
+++ src/components/RegionTreeModal.vue
@@ -4,7 +4,18 @@
     v-model="show"
     @ok="handleOk"
   >
-    <ul class="ztree" style="margin-left: 20px" id="regionTree"></ul>
+    <a-input-search
+      placeholder="请输入区域名称"
+      @search="onSearch"
+      enterButton
+    />
+      <div v-show="noData" style="display: flex;align-items: center;justify-content: space-between;flex-direction: column;position: absolute;top: 160px;left: 50%;transform: translateX(-50%)">
+        <img src="../assets/no-data.svg"/>
+        <span style="color: rgba(0,0,0,.3);margin-top: 6px">暂无匹配数据</span>
+      </div>
+    <a-spin :spinning="loading">
+      <ul class="ztree" style="margin:10px 0 0 10px;height: 300px;overflow-y: auto;" id="regionTree"></ul>
+    </a-spin>
   </a-modal>
 </template>
 
@@ -26,6 +37,8 @@
     data() {
       return {
         isTreeInit:false,
+        loading:true,
+        noData:false,
         treeSetting: {
           async: {
             enable: true,
@@ -52,7 +65,9 @@
     },
     methods: {
       initTree() {
-        get(`${BASE_URL}/information/v1/administrativeRegion/regionTree?regionCode=120103000000`).then(res => {
+        this.loading=true;
+        get(`${BASE_URL}/information/v1/administrativeRegion/regionTree?regionCode=120100000000`).then(res => {
+          this.loading=false;
           if (res.resCode === 1) {
             $.fn.zTree.init($('#regionTree'), this.treeSetting, res.data);
             this.isTreeInit=true;
@@ -60,21 +75,42 @@
         })
       },
       asyncGetRegionNodesSuccess(event, treeId, treeNode, res) {
-        const treeObj = $.fn.zTree.getZTreeObj(treeId);
-        treeObj.addNodes(treeNode, -1,res.data);
-        if (treeNode.children[0].resCode === 1) {
-          treeObj.removeNode(treeNode.children[0]);
-          treeObj.updateNode(treeNode);
-        }
+        // 对展开节点引发的请求进行处理
+         if(treeNode){
+           const treeObj = $.fn.zTree.getZTreeObj(treeId);
+           treeObj.addNodes(treeNode, -1,res.data);
+           if (treeNode.children[0].resCode === 1) {
+             treeObj.removeNode(treeNode.children[0]);
+             treeObj.updateNode(treeNode);
+           }
+         }
       },
       handleOk() {
         const selectedNodes=$.fn.zTree.getZTreeObj('regionTree').getSelectedNodes();
         this.$emit('getRegion',selectedNodes[0]);
         this.$emit('change', false)
+      },
+      onSearch(e){
+        this.searchRegion(e);
+      },
+      searchRegion(regionName){
+        this.loading=true;
+        $.fn.zTree.destroy('regionTree');
+        get(`${BASE_URL}/information/v1/administrativeRegion/regionTree?regionCode=120100000000&regionName=${regionName}`).then(res => {
+          this.loading=false;
+          if (res.resCode === 1) {
+            if(res.data.length!==0){
+              this.noData=false;
+              $.fn.zTree.init($('#regionTree'), this.treeSetting, res.data);
+            }else {
+              this.noData=true
+            }
+          }
+        })
       }
     },
     watch:{
-      visible(newVal,oldVal){
+      visible(newVal){
         if(!this.isTreeInit&&newVal){
           this.initTree();
         }
